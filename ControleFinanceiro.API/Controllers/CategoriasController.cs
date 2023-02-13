5c08aceb77a9509442465cfdfd9d7e185cc0ac86
Luiz Fernando <lfernando488@gmail.com>
2023-02-12 22:43:42 -0300

    first commit

---
+++ ControleFinanceiro.API/Controllers/CategoriasController.cs
@@ -0,0 +1,101 @@
+﻿using ControleFinanceiro.BLL.Models;
+using ControleFinanceiro.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleFinanceiro.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+
+        private readonly Contexto _contexto;
+
+        public CategoriasController(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Categoria>>> GetCategorias()
+        {
+            return await _contexto.Categorias.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Categoria>> GetCategoria(int id)
+        {
+            var categoria = await _contexto.Categorias.FindAsync(id);
+
+            if(categoria == null)
+            {
+                return NotFound();
+            }
+            return (categoria);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategoria(int id, Categoria categoria)
+        {
+            if (id != categoria.CategoriaId)
+            {
+                return BadRequest();
+            }
+
+            _contexto.Entry(categoria).State = EntityState.Modified;
+
+            try
+            {
+                await _contexto.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoriaExists(id))
+                {
+                    return NotFound();
+                }
+                else 
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Categoria>> PostCategoria(Categoria categoria)
+        {
+            _contexto.Categorias.Add(categoria);
+            await _contexto.SaveChangesAsync();
+
+            return CreatedAtAction("GetCategoria", new { Id = categoria.CategoriaId }, categoria);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Categoria>> DeleteCategoria(int id)
+        {
+            var categoria = await _contexto.Categorias.FindAsync(id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            _contexto.Categorias.Remove(categoria);
+            await _contexto.SaveChangesAsync();
+            return categoria;
+        }
+
+        private bool CategoriaExists(int id) 
+        {
+            return _contexto.Categorias.Any(c => c.CategoriaId == id);
+        }
+    }
+}
